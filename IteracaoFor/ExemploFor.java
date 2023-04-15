ba41f1ac72ec80c87d7ba2858e71a8c5945ad602
CaioSass <caio.csc@hotmail.com.br>
2023-04-14 21:22:05 -0300

    14/04/2023

---
+++ IteracaoFor/ExemploFor.java
@@ -0,0 +1,83 @@
+package IteracaoFor;
+
+import java.util.Scanner;
+
+public class ExemploFor {
+    Scanner sc = new Scanner(System.in);
+    private int j;
+
+    public void exemplo1() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("o º da iteração é " + i);
+        }
+    }
+
+    public void exemplo2() {
+        int vetor[] = new int[10];
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("vetor[" + 1 + "]=");
+            vetor[i] = sc.nextInt();
+
+        }
+        for (int i = 9; i >= 0; i--) {
+            System.out.println("vetor[" + i + "]=" + vetor[i]);
+        }
+    }
+
+    public void exemplo3() {
+        double carrinhoCompra[] = new double[] { 19.90, 35.70, 56.80, 97.50 };
+        double valorFinal = 0;
+        for (int i = 0; i < carrinhoCompra.length; i++) {
+            valorFinal += carrinhoCompra[i];
+            System.out.println(" Valor Parcial Compra: R$" + valorFinal);
+        }
+    }
+
+    public void exemplo4() {
+        double vetorNotas[] = new double[4];
+        for (int i = 0; i < vetorNotas.length; i++) {
+            System.out.println("Digite a Nota" + (1 + 1) + ":");
+            vetorNotas[i] = sc.nextDouble();
+
+        }
+        double mediaNotas = 0;
+        for (int i = 0; i < vetorNotas.length; i++) {
+            mediaNotas += vetorNotas[i];
+            System.out.println("A" + (i + 1) + "ª Nota é " + vetorNotas[i]);
+
+        }
+        mediaNotas /= vetorNotas.length;
+        System.out.println(("A média do Aluno é" + mediaNotas));
+    }
+
+    /**
+     * 
+     */
+    public void exemplo5() {
+        double vetorMedia[] = new double[10];
+        for (int i = 0; i < vetorMedia.length; i++) {
+            double Notas = 0;
+            for (int j = 0; j < 4; j++) {
+
+                System.out.println("Nota " + (j + 1) + "Aluno" + (j + 1) + ":");
+                Notas += sc.nextDouble();
+
+            }
+            Notas /= 4;
+            vetorMedia[i] = Notas;
+        
+        }
+        for (int i = 0; i < vetorMedia.length; i++) {
+            if(vetorMedia[i]>7){
+            System.out.println("A média do aluno"+(i+1)+" é "+vetorMedia[i]);
+
+    
+            }
+        }
+    }
+    public void exemplo6() {
+        int vetor = 
+    }
+}
+
+
